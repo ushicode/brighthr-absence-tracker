0b6436ff3d514321cb1f888eb88a1cb4cc4c58ad
Collins Ushi <collinsushi@gmail.com>
2025-08-29 13:33:08 +0100

    add tanstack query to fetch from api-service

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import type React from 'react'
 import './App.css'
 import AbsenceTable from './components/AbsenceTable'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient: QueryClient = new QueryClient();
 
 const App: React.FC = () => {
 
+
   return (
-    <div>
-      <header>
-        <h1>BrightHR Absence Tracker</h1>
-      </header>
-     <main>
-       <AbsenceTable />
-     </main>
-     <footer>
-       <p>© 2024 BrightHR</p>
-     </footer>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <header>
+          <h1>BrightHR Absence Tracker</h1>
+        </header>
+        <main>
+          <AbsenceTable />
+        </main>
+        <footer>
+          <p>© 2024 BrightHR</p>
+        </footer>
+      </div>
+    </QueryClientProvider>
   )
 }
 
--- src/components/AbsenceTable.tsx
+++ src/components/AbsenceTable.tsx
@@ -1,32 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { ApiService } from '../services/api-service';
+import { useQuery } from '@tanstack/react-query';
 
 const AbsenceTable: React.FC = () => {
-  return (
-    <div>
-        <table>
-        <thead>
-            <tr>
-                <th>Start Date</th>
-                <th>End Date</th>
-                <th>Employee Name</th>
-                <th>Status</th>
-                <th>Absence Type</th>
-                <th>Conflict</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>2024-06-01</td>
-                <td>2024-06-05</td>
-                <td>Firstname lastname</td>
-                <td>Approved</td>
-                <td>Vacation</td>
-                <td>X</td>
-            </tr>
-        </tbody>
-    </table>
-    </div>
-  )
+
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['absences'],
+        queryFn: ApiService.getAbsences
+    });
+
+    console.log(data);
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Employee Name</th>
+                        <th>Status</th>
+                        <th>Absence Type</th>
+                        <th>Conflict</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2024-06-01</td>
+                        <td>2024-06-05</td>
+                        <td>Firstname lastname</td>
+                        <td>Approved</td>
+                        <td>Vacation</td>
+                        <td>X</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default AbsenceTable;
